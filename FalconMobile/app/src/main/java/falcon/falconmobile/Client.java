85418677445e7b5642ec7066ce881f963e420cac
MessasKouseila <k_messas@yahoo.fr>
2017-12-23 20:09:03 +0100

    streaming ok

---
+++ FalconMobile/app/src/main/java/falcon/falconmobile/Client.java
@@ -0,0 +1,24 @@
+package falcon.falconmobile;
+
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+/**
+ * Created by loubia on 23/12/17.
+ */
+class Client extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+    }
+}
--- FalconMobile/app/src/main/java/falcon/falconmobile/MainActivity.java
+++ FalconMobile/app/src/main/java/falcon/falconmobile/MainActivity.java
@@ -1,13 +1,83 @@
 package falcon.falconmobile;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.niqdev.mjpeg.DisplayMode;
+import com.github.niqdev.mjpeg.Mjpeg;
+import com.github.niqdev.mjpeg.MjpegSurfaceView;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int TIMEOUT = 5;
+    MjpegSurfaceView mjpegView;
+    private TextView mTextViewAngleLeft;
+    private TextView mTextViewStrengthLeft;
+
+// ok http
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
+        mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
+
+        mjpegView = (MjpegSurfaceView) findViewById(R.id.mjpegViewDefault);
+
+        JoystickView joystickLeft = (JoystickView) findViewById(R.id.joystickView_left);
+        joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleLeft.setText(angle + "°");
+                mTextViewStrengthLeft.setText(strength + "%");
+            }
+        });
+    }
+
+
+    private DisplayMode calculateDisplayMode() {
+        int orientation = getResources().getConfiguration().orientation;
+        return orientation == Configuration.ORIENTATION_LANDSCAPE ?
+                DisplayMode.FULLSCREEN : DisplayMode.BEST_FIT;
+    }
+
+    private void loadIpCam() {
+
+        Mjpeg.newInstance()
+                .credential("falcon", "falcon")
+                //.credential(getPreference(""), getPreference(""))
+                //.open("http://10.3.141.1:8080/html/cam_pic_new.php", TIMEOUT)
+                .open("http://10.188.54.168:8080/html/cam_pic_new.php", TIMEOUT)
+                .subscribe(
+                        inputStream -> {
+                            mjpegView.setSource(inputStream);
+                            mjpegView.setDisplayMode(calculateDisplayMode());
+                            mjpegView.showFps(true);
+                        },
+                        throwable -> {
+                            Log.e(getClass().getSimpleName(), "mjpeg error", throwable);
+                            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+                        });
+    }
+
+
+    /*
+        updateWeather = new Update();
+        updateWeather.execute(MainActivity.listCity);
+    */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadIpCam();
     }
 }
